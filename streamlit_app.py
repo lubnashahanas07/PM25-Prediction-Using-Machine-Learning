046b621a2792a5dff99cb840604c485c8c9ff80a
lubnashahanas07 <lubnashahanas07@gmail.com>
2026-09-17 17:01:07 +0530

    few updations in streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,43 @@
 import pandas as pd
 import streamlit as st
 import pickle
+from PIL import Image
 
 with open("xgboost_model.sav","rb") as file:
     model = pickle.load(file)
 
-st.title("PM2.5 Prediction")
+image = Image.open("pm25_banner.jpeg")
+st.image(image,use_container_width=True)
+#st.title("PM2.5 Prediction")
 
 st.subheader("Enter Atmospheric and Meteorological Values")
 
-co = st.number_input("CO")
-aermr04 = st.number_input("Aerosol AERMR04")
-aermr05 = st.number_input("Aerosol AERMR05")
-aermr06 = st.number_input("Aerosol AERMR06")
-aermr09 = st.number_input("Aerosol AERMR09")
-aermr07 = st.number_input("Aerosol AERMR07")
-aermr10 = st.number_input("Aerosol AERMR10")
-aermr08 = st.number_input("Aerosol AERMR08")
-no2 = st.number_input("NO2")
-no = st.number_input("NO")
-go3 = st.number_input("Ozone (GO3)")
-so2 = st.number_input("SO2")
-q = st.number_input("Specific Humidity (Q)")
-t = st.number_input("Temperature (T)")
-u10 = st.number_input("U10")
-v10 = st.number_input("V10")
-d2m = st.number_input("2m Dew Point (D2M)")
-t2m = st.number_input("2m Temperature (T2M)")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    co = st.number_input("CO")
+    aermr04 = st.number_input("Aerosol AERMR04")
+    aermr05 = st.number_input("Aerosol AERMR05")
+    aermr06 = st.number_input("Aerosol AERMR06")
+    aermr09 = st.number_input("Aerosol AERMR09")
+    aermr07 = st.number_input("Aerosol AERMR07")
+
+with col2:
+    aermr10 = st.number_input("Aerosol AERMR10")
+    aermr08 = st.number_input("Aerosol AERMR08")
+    no2 = st.number_input("NO2")
+    no = st.number_input("NO")
+    go3 = st.number_input("Ozone (GO3)")
+    so2 = st.number_input("SO2")
+
+with col3:
+    q = st.number_input("Specific Humidity (Q)")
+    t = st.number_input("Temperature (T)")
+    u10 = st.number_input("U10")
+    v10 = st.number_input("V10")
+    d2m = st.number_input("2m Dew Point (D2M)")
+    t2m = st.number_input("2m Temperature (T2M)")
+
 
 comp_met_variables = [
     'co',

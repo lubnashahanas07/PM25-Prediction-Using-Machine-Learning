2cb6a3a527ae2eb8d3f3b8f0eaa27c8ea8339407
lubnashahanas07 <lubnashahanas07@gmail.com>
2026-09-18 14:07:10 +0530

    few corrections in ppt

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,29 +15,28 @@ st.subheader("Enter Atmospheric and Meteorological Values")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    co = st.number_input("CO")
-    aermr04 = st.number_input("Aerosol AERMR04")
-    aermr05 = st.number_input("Aerosol AERMR05")
-    aermr06 = st.number_input("Aerosol AERMR06")
-    aermr09 = st.number_input("Aerosol AERMR09")
-    aermr07 = st.number_input("Aerosol AERMR07")
+    co = st.number_input("Carbon Monoxide (CO)")
+    aermr04 = st.number_input("Dust Aerosol (0.03–0.55 µm)")
+    aermr05 = st.number_input("Dust Aerosol (0.55–0.9 µm)")
+    aermr06 = st.number_input("Dust Aerosol (0.9–20 µm)")
+    aermr09 = st.number_input("Hydrophilic Black Carbon")
+    aermr07 = st.number_input("Hydrophilic Organic Matter")
 
 with col2:
-    aermr10 = st.number_input("Aerosol AERMR10")
-    aermr08 = st.number_input("Aerosol AERMR08")
-    no2 = st.number_input("NO2")
-    no = st.number_input("NO")
-    go3 = st.number_input("Ozone (GO3)")
-    so2 = st.number_input("SO2")
+    aermr10 = st.number_input("Hydrophobic Black Carbon")
+    aermr08 = st.number_input("Hydrophobic Organic Matter")
+    no2 = st.number_input("Nitrogen Dioxide (NO₂)")
+    no = st.number_input("Nitrogen Monoxide (NO)")
+    go3 = st.number_input("Ozone (O₃)")
+    so2 = st.number_input("Sulphur Dioxide (SO₂)")
 
 with col3:
-    q = st.number_input("Specific Humidity (Q)")
-    t = st.number_input("Temperature (T)")
-    u10 = st.number_input("U10")
-    v10 = st.number_input("V10")
-    d2m = st.number_input("2m Dew Point (D2M)")
-    t2m = st.number_input("2m Temperature (T2M)")
-
+    q = st.number_input("Specific Humidity")
+    t = st.number_input("Temperature")
+    u10 = st.number_input("10 m U-Wind Component")
+    v10 = st.number_input("10 m V-Wind Component")
+    d2m = st.number_input("2 m Dew Point Temperature")
+    t2m = st.number_input("2 m Temperature")
 
 comp_met_variables = [
     'co',
